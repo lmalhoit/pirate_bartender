6a0cf691b583baa5468b0cfb4cac41657d18a0cc
Lauren Malhoit <laurenmalhoit@gmail.com>
2015-01-17 18:49:18 +0000

    Initial Commit

---
+++ pirate_bartender.py
@@ -0,0 +1,26 @@
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
+for key,value in questions.iteritems():
+  print value
+  answer = raw_input()
+  if answer == "y" or answer == "yes":
+    answer = True
+  else:
+    answer = False
+  answers = {key:answer}
+  
+print answers
